4ed5bf79d7f216e36c66059913ef7ea09e706ddc
manuelm11-bit <manuel.miranda.t0129866@edu.atec.pt>
2025-10-10 00:05:38 -0700

    Update ex1.py

--- Dário/python/exercicios/ex1.py
+++ Dário/python/exercicios/ex1.py
@@ -1,7 +1,8 @@
-segundos = int(input("Digite os segundos"))
+segundos = int(input("Digite o tempo em segundos: "))
 
 horas = segundos // 3600
-minutos = segundos // 60
-segundos = segundos // 1
+resto = segundos % 3600
+minutos = resto // 60
+segundo = resto % 60
 
-print()
+print(f"segundoss equivalem a {horas}h {minutos}m {segundos}s")
